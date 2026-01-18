6a372982f4b64a23c49702426b13728bb42e0814
Ryui-code <ulanbektalasbaev2020@gmail.com>
2026-01-18 16:03:03 +0600

    Hi!

--- settings/instagram/serializers.py
+++ settings/instagram/serializers.py
@@ -8,16 +8,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ("id", "username", "email", "password")
+        fields = ('username', 'password', 'avatar', 'bio', 'hashtag', 'link', 'status', 'registered_date')
 
     def create(self, validated_data):
-        user = UserProfile.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data.get("email"),
-            password=validated_data["password"]
-        )
+        user = UserProfile.objects.create_user(**validated_data)
         return user
-
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
